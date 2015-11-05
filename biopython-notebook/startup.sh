3799ea422c8d99fb4b0b85ecb65738d6b39b7474
Tao Zhang <forrestzhang1982@gmail.com>
2015-11-05 10:30:10 -0600

    add gid

--- biopython-notebook/startup.sh
+++ biopython-notebook/startup.sh
@@ -6,6 +6,7 @@ NC='\033[0m'
 
 if ! id -u $JUPYTER_USER > /dev/null 2>&1; then
   echo -e "use local user: " ${BLUE}$JUPYTER_USER${NC}
+  addgroup --gid $JUPYTER_UID $JUPYTER_USER
   adduser --uid  $JUPYTER_UID  -s /bin/bash --disabled-password --home $JUPYTER_HOME $JUPYTER_USER
 fi
 
@@ -13,6 +14,7 @@ if [ -e $JUPYTER_HOME ];
   then
     echo -e ${GREEN}$JUPYTER_HOME${NC} exists
     chown -R $JUPYTER_UID $JUPYTER_HOME
+    chgrp -R $JUPYTER_UID $JUPYTER_HOME
 else
   mkdir -p $JUPYTER_HOME
   chown -R $JUPYTER_UID $JUPYTER_HOME
